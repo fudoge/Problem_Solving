1ac3fe67a340c37295fe1dd632bdbfcc6cb5468a
fudoge <77766067+fudoge@users.noreply.github.com>
2023-10-25 17:06:07 +0900

    [level 3] Title: 숫자 타자 대회, Time: 52.68 ms, Memory: 48.1 MB -BaekjoonHub

--- 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
+++ 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAX = 1e5 + 1;
-
-const int NUMBERRANGE = 10;
-
-const int weight[NUMBERRANGE][NUMBERRANGE] = {
+const int weight[10][10] = {
     { 1, 7, 6, 7, 5, 4, 5, 3, 2, 3 },
     { 7, 1, 2, 4, 2, 3, 5, 4, 5, 6 },
     { 6, 2, 1, 2, 3, 2, 3, 5, 4, 5 },
@@ -18,7 +14,7 @@ const int weight[NUMBERRANGE][NUMBERRANGE] = {
     { 3, 6, 5, 4, 5, 3, 2, 4, 2, 1 }
 };
 
-int dp[MAX][NUMBERRANGE][NUMBERRANGE];
+int dp[100001][10][10];
 
 string nums;
 
@@ -36,6 +32,7 @@ int solve(int idx, int left, int right) {
         return result = 1 + solve(idx + 1, left, right);
     }
 
+    //손가락 움직여보자..
     return result = min(solve(idx + 1, target, right) + weight[left][target]
         , solve(idx + 1, left, target) + weight[right][target]);
 }
