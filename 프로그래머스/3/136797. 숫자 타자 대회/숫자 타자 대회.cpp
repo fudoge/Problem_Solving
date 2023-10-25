89e602b786301e875df44ee5d14a7175cc76913a
fudoge <77766067+fudoge@users.noreply.github.com>
2023-10-25 16:12:27 +0900

    [level 3] Title: 숫자 타자 대회, Time: 51.38 ms, Memory: 48 MB -BaekjoonHub

---
+++ 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1e5 + 1;
+
+const int NUMBERRANGE = 10;
+
+const int weight[NUMBERRANGE][NUMBERRANGE] = {
+    { 1, 7, 6, 7, 5, 4, 5, 3, 2, 3 },
+    { 7, 1, 2, 4, 2, 3, 5, 4, 5, 6 },
+    { 6, 2, 1, 2, 3, 2, 3, 5, 4, 5 },
+    { 7, 4, 2, 1, 5, 3, 2, 6, 5, 4 },
+    { 5, 2, 3, 5, 1, 2, 4, 2, 3, 5 },
+    { 4, 3, 2, 3, 2, 1, 2, 3, 2, 3 },
+    { 5, 5, 3, 2, 4, 2, 1, 5, 3, 2 },
+    { 3, 4, 5, 6, 2, 3, 5, 1, 2, 4 },
+    { 2, 5, 4, 5, 3, 2, 3, 2, 1, 2 },
+    { 3, 6, 5, 4, 5, 3, 2, 4, 2, 1 }
+};
+
+int dp[MAX][NUMBERRANGE][NUMBERRANGE];
+
+string nums;
+
+int solve(int idx, int left, int right) {
+    if (idx == nums.length()) return 0;
+    int& result = dp[idx][left][right];
+    if (result != -1) {
+        return result;
+    }
+    
+    int target = nums[idx] - '0';
+
+    //손가락 움직일필요 없음
+    if (left == target || right == target) {
+        return result = 1 + solve(idx + 1, left, right);
+    }
+
+    return result = min(solve(idx + 1, target, right) + weight[left][target]
+        , solve(idx + 1, left, target) + weight[right][target]);
+}
+
+int solution(string numbers) {
+    memset(dp, -1, sizeof(dp));
+    nums = numbers;
+
+    return solve(0, 4, 6);
+}
