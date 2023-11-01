e7f71e016fd528e1d752975e4954ae46daaa15a3
fudoge <77766067+fudoge@users.noreply.github.com>
2023-11-01 17:12:19 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -3,24 +3,26 @@
 using namespace std;
 
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
-    sort(data.begin(), data.end(), [col](const auto& a, const auto& b) {
-        if (a[col - 1] == b[col - 1]) {
+    sort(data.begin(), data.end(), [col](const auto& a, const auto &b) {
+        if(a[col-1] == b[col-1]) {
             return a[0] > b[0];
         }
-        return a[col - 1] < b[col - 1];
+        return a[col-1] < b[col-1];
     });
-
-    vector<int> sums;
-    int num = 0;
-
-    for (int i = row_begin - 1; i < row_end; i++) {
-        int sum = 0;
-        for (int j = 0; j < data[i].size(); j++) {
-            sum += data[i][j] % (i + 1);
+    
+    vector<int> nums;
+    for(int i = row_begin-1; i < row_end; i++) {
+        int x = 0;
+        for(const auto &num : data[i]) {
+            x += num%(i+1);
         }
-        sums.push_back(sum);
-        num ^= sum;
+        nums.push_back(x);
     }
-
-    return num;
+    
+    int answer = nums[0];
+    for(int i = 1; i < nums.size(); i++) {
+        answer = answer ^ nums[i];
+    }
+    
+    return answer;
 }
