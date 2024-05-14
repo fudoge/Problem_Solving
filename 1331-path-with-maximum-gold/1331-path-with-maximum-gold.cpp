2bca6209787ba88944c1976d1d22f5fdc11da740
fudoge <77766067+fudoge@users.noreply.github.com>
2024-05-14 20:09:57 +0900

    Time: 251 ms (30.66%), Space: 9.8 MB (47.89%) - LeetHub

---
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.cpp
@@ -0,0 +1,43 @@
+int dr[] = {-1, 1, 0, 0};
+int dc[] = {0, 0, -1, 1};
+
+class Solution {
+private:
+    int maxGold;
+    int m;
+    int n;
+public:
+    void dfs(int sum, int row, int col, vector<vector<bool>>& visited, vector<vector<int>>& grid) {
+        sum += grid[row][col];
+        maxGold = max(maxGold, sum);
+        visited[row][col] = true;
+        for(int i = 0; i < 4; i++) {
+            int nr = dr[i] + row;
+            int nc = dc[i] + col;
+
+            if(nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+            if(visited[nr][nc]) continue;
+            if(!grid[nr][nc]) continue;
+            dfs(sum, nr, nc, visited, grid);
+        }
+
+        visited[row][col] = false;
+    }
+
+    int getMaximumGold(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        maxGold = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j]) {
+                    dfs(0, i, j, visited, grid);
+                }
+            }
+        }
+
+        return maxGold;
+    }
+};
