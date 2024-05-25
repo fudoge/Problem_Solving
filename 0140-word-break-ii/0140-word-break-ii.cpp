4825b6216d77f8aa276a6c35df74d6164e9763af
fudoge <77766067+fudoge@users.noreply.github.com>
2024-05-25 12:01:11 +0900

    Time: 0 ms (100%), Space: 8.4 MB (99.81%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,24 +1,29 @@
 class Solution {
+private:
+    int n;
 public:
-    bool check(string &s, string &word, int idx) {
+    bool check(string &s, string &word, int idx, int wordlen) {
         int i = 0;
-        while(i < word.size()) {
-            if(i + idx >= s.size() || s[i + idx] != word[i]) return false;
+        while(i < wordlen) {
+            if(i + idx >= n || s[i + idx] != word[i]) return false;
             i++;
         }
 
         return true;
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        //dp[idx] idx번부터 인식한 뒤의 suffix들.
-        //ex) s = "~~dog", word Dict = ["dog", ...] => dp[n-3] = {"dog"}.
-        int n = s.size();
+        // dp[idx] = idx번부터 인식한 뒤의 suffix들.
+        // ex) s = "~~dog", word Dict = ["dog", ...] => dp[n-3] = {"dog ", ...}.
+        // 마지막 공백은 지워주면 된다.
+        
+        n = s.size();
         vector<vector<string>> dp(n+1, vector<string>());
         dp[n] = {""};
         for(int i = n-1; i >= 0; i--) {
             for(string &word : wordDict) {
-                if(check(s, word, i)) {
-                    for(string suffix : dp[i + word.size()]) {
+                int wordlen = word.size();
+                if(check(s, word, i, wordlen)) {
+                    for(string suffix : dp[i + wordlen]) {
                         dp[i].push_back(word + " " + suffix);
                     }
                 }
