f57d60f7fa3b0816311fa115cda58bcb90b7f6b7
fudoge <77766067+fudoge@users.noreply.github.com>
2024-05-30 13:31:09 +0900

    Time: 0 ms (100%), Space: 9.4 MB (36.3%) - LeetHub

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -3,27 +3,15 @@ public:
     int countTriplets(vector<int>& arr) {
         int n = arr.size();
         int ans = 0;
-        int length = 2;
+        vector<int> prefix(n+1, 0);
+        for(int i = 1; i < n+1; i++) {
+            prefix[i] = prefix[i-1] ^ arr[i-1];
+        }
 
-        while(length <= n) {
-            for(int start = 0; start <= n - length; start++) {
-                int a = 0, b, i;
-                for(i = start; i < start + length - 1; i++) {
-                    a ^= arr[i];
-                }
-                b = arr[i];
-
-                // cout << a << "(a) " << b << "(b) " << start << "(i) " << i << "(j) " << start + length -1 << "(k)\n";
-                if(a == b) ans++;
-                for(i = i-1; i > start; i--) {
-                    a ^= arr[i];
-                    b ^= arr[i];
-                    if(a == b) ans++;
-                    // cout << a << "(a) " << b << "(b) " << start << "(i) " << i << "(j) " << start + length -1 << "(k)\n";
-                }
+        for(int i = 0; i < n; i++) {
+            for(int k = i+1; k < n; k++) {
+                if(prefix[i] == prefix[k+1]) ans += k-i;
             }
-            length++;
-            // cout << endl;
         }
 
         return ans;
