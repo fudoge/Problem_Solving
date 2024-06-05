b852eeeb63d0dd271b3f706a0bd0e71d4e815f60
fudoge <77766067+fudoge@users.noreply.github.com>
2024-06-05 10:28:41 +0900

    Time: 64 ms (99.45%), Space: 120.4 MB (29.05%) - LeetHub

---
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("03", "unroll-loops");
+
+static const int __ = [](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        const int n = nums.size();
+        long long ans = 0;
+        int maxNum = *max_element(nums.begin(), nums.end());
+        int freq = 0;
+        int limit = 0;
+        int right = 0;
+
+        while(right < n) {
+            if(nums[right] == maxNum) freq++;
+            if(freq == k) break;
+            right++;
+        }
+
+        if(freq < k) return 0;
+
+        while(right < n) {
+            bool found = false;
+            if(nums[right] == maxNum) {
+                found = true;
+            }
+
+            if(found) {
+                while(limit < right && nums[limit] != maxNum) {
+                    limit++;
+                }
+                limit++;
+            }
+
+            ans += limit;
+            right++;   
+        }
+
+        return ans;
+    }
+};
