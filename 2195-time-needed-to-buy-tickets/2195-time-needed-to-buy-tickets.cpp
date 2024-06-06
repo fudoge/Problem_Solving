82e38938751b5a9dc416c077d69429d9699c96b6
fudoge <77766067+fudoge@users.noreply.github.com>
2024-06-06 11:21:16 +0900

    Time: 4 ms (40%), Space: 9.2 MB (98.76%) - LeetHub

---
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time = 0;
+        int n = tickets.size();
+        while(tickets[k] > 0) {
+            for(int i = 0; i < n; i++) {
+                if(tickets[i] <= 0) continue;
+                tickets[i]--;
+                time++;
+
+                if(i == k && tickets[k] <= 0) break;
+            }
+        }
+
+        return time;
+    }
+};
