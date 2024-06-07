acf9cf45f9cc4b87fbb07350dd829e073096ee06
fudoge <77766067+fudoge@users.noreply.github.com>
2024-06-07 13:32:41 +0900

    Time: 44 ms (97.91%), Space: 71.6 MB (74.65%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,34 +1,76 @@
-#pragma GCC optimize("03", "unroll-loops");
+#pragma GCC optimize("03", "unroll-loops")
 static const int __ = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     return 0;
 }();
+
+class TrieNode {
+public:
+    bool isEnd;
+    TrieNode* children[26];
+    
+    TrieNode() : isEnd(false) {
+        fill(begin(children), end(children), nullptr);
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(const string& word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children[c - 'a'] == nullptr) {
+                node->children[c - 'a'] = new TrieNode();
+            }
+            node = node->children[c - 'a'];
+        }
+        node->isEnd = true;
+    }
+    
+    string getPrefix(const string& word) {
+        TrieNode* node = root;
+        string prefix = "";
+        for (char c : word) {
+            if (node->children[c - 'a'] == nullptr) {
+                break;
+            }
+            node = node->children[c - 'a'];
+            prefix += c;
+            if (node->isEnd) {
+                return prefix;
+            }
+        }
+        return word;
+    }
+};
 
 class Solution {
 public:
     string replaceWords(vector<string>& dictionary, string sentence) {
-        string ans = "";
-        int i = 0;
-
-        while(i < sentence.size()) {
-            string prefix = "";
-            while(i < sentence.size() && sentence[i] != ' ') {
-                prefix += sentence[i];
-                if(find(dictionary.begin(), dictionary.end(), prefix) != dictionary.end()) {
-                    while(i < sentence.size() && sentence[i] != ' ') i++;
-                    break;
-                }
-                i++;
-            }
-
-            ans += prefix;
-            ans += " ";
-            i++;
+        Trie trie;
+        for (const string& word : dictionary) {
+            trie.insert(word);
         }
-
-        ans.pop_back();
+        
+        stringstream ss(sentence);
+        string word;
+        string ans = "";
+        
+        while (ss >> word) {
+            if (!ans.empty()) {
+                ans += " ";
+            }
+            ans += trie.getPrefix(word);
+        }
+        
         return ans;
     }
 };
