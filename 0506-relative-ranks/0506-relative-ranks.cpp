235488e5631ad69f88ff3f1d91a53c8c83bd429c
fudoge <77766067+fudoge@users.noreply.github.com>
2024-06-07 13:47:44 +0900

    Time: 3 ms (96.72%), Space: 13.9 MB (61.75%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,3 +1,11 @@
+#pragma GCC optimize("03", "unroll-loops");
+static const int __ = [](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
 struct comp {
     bool operator()(const auto &a, const auto &b){
         return a.first < b.first;
