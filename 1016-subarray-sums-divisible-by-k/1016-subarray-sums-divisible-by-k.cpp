9df9c3c82df11ad0a0e64126d2a0436591acd4f4
fudoge <77766067+fudoge@users.noreply.github.com>
2024-06-09 10:10:38 +0900

    Time: 42 ms (51.83%), Space: 35.1 MB (44.61%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prefixSums(n + 1, 0);
-
-        for(int i = 0; i < n; i++) {
-            prefixSums[i + 1] = (prefixSums[i] + nums[i]) % k;
-            if (prefixSums[i + 1] < 0) {
-                prefixSums[i + 1] += k;
-            }
+        nums[0] %= k;
+        if(nums[0] < 0) nums[0] += k;
+        for(int i = 1; i < n; i++) {
+            nums[i] = (nums[i] + nums[i-1]) % k;
+            if(nums[i] < 0) nums[i] += k;
         }
-
         int ans = 0;
-        unordered_map<int, int> countMap;
-        countMap[0] = 1;
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        for(int i = 0; i < n; i++) {
+            ans += mp[nums[i]];
 
-        for(int i = 1; i <= n; i++) {
-            ans += countMap[prefixSums[i]];
-            countMap[prefixSums[i]]++;
+            mp[nums[i]]++;
         }
 
         return ans;
