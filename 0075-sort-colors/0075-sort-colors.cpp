dc1a7cc33d3d5629f360730a81ed9795d10d82e3
fudoge <77766067+fudoge@users.noreply.github.com>
2024-06-12 09:50:01 +0900

    Time: 4 ms (31.04%), Space: 9.7 MB (71.79%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int red = 0;
-        int white = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == 0) red++;
-            else if(nums[i] == 1) white++;
-        }
+        int left = 0;
+        int right = nums.size()-1;
+        int mid = 0;
 
-        for(int i = 0; i < red; i++) {
-            nums[i] = 0;
-        }
-        for(int i = red; i < red + white; i++) {
-            nums[i] = 1;
-        }
-        for(int i = red + white; i < nums.size(); i++) {
-            nums[i] = 2;
+        while(mid <= right) {
+            if(nums[mid] == 0) {
+                swap(nums[left], nums[mid]);
+                left++;
+                mid++;
+            } else if(nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[right]);
+                right--;
+            }
         }
     }
 };
