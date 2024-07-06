7767516f011e6d9f30f271d432630bddad0f052f
fudoge <77766067+fudoge@users.noreply.github.com>
2024-07-06 15:14:06 +0900

    Time: 2 ms (54.14%), Space: 7 MB (71.18%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -1,28 +1,7 @@
-#pragma GCC optimize("03", "unroll-loops");
-static const int __ = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
-
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int pillow = 1;
-        bool dirFlag = true;
-
-        while(time > 0) {
-            if(dirFlag) {
-                pillow++;
-                if(pillow == n) dirFlag = !dirFlag;
-            } else {
-                pillow--;
-                if(pillow == 1) dirFlag = !dirFlag;
-            }
-            time--;
-        }
-
-        return pillow;
+        int N = 2 * (n-1), x = time % N;
+        return 1+(x < n? x : N-x);
     }
 };
