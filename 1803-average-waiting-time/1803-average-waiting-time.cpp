b043e00fb62c9cd631c24d1d02f62ac973543d74
fudoge <77766067+fudoge@users.noreply.github.com>
2024-07-09 10:28:26 +0900

    Time: 123 ms (98.82%), Space: 76.7 MB (22.35%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.cpp
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -1,3 +1,11 @@
+#pragma GCC optimize("03", "unroll-loops");
+static const int __ = [](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
@@ -5,10 +13,7 @@ public:
         int currTime = customers[0][1] + customers[0][0];
         long long sum = customers[0][1];
         for(int i = 1; i < n; i++) {
-            if(currTime > customers[i][0]) {
-                sum += currTime - customers[i][0];
-            }
-            sum += customers[i][1];
+            sum += customers[i][1] + (currTime > customers[i][0]? currTime - customers[i][0] : 0);
             currTime = max(currTime, customers[i][0]) + customers[i][1];
         }
 
