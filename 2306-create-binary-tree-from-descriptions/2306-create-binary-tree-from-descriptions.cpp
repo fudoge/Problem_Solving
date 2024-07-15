bc3928141d2d47430f9dd8a8f7a9746b78ccf4c1
fudoge <77766067+fudoge@users.noreply.github.com>
2024-07-15 10:26:43 +0900

    Time: 688 ms (64.85%), Space: 276.3 MB (59.39%) - LeetHub

---
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> mp;
+        unordered_set<int> children;
+        
+        for(const auto &description : descriptions) {
+            int parentNum = description[0];
+            int childNum = description[1];
+            int isLeft = description[2];
+
+            if(mp.find(childNum) == mp.end()) {
+                mp[childNum] = new TreeNode(childNum);
+            }
+
+            if(mp.find(parentNum) == mp.end()) {
+                mp[parentNum] = new TreeNode(parentNum);
+            }
+
+            if(isLeft) {
+                mp[parentNum]->left = mp[childNum];
+            } else {
+                mp[parentNum]->right = mp[childNum];
+            }
+
+            children.insert(childNum);
+        }
+
+        TreeNode* root;;
+        for(const auto &description : descriptions) {
+            int parentNum = description[0];
+            if(children.find(parentNum) == children.end()) {
+                root = mp[parentNum];
+                break;
+            }
+        }
+
+        return root;
+    }
+};
