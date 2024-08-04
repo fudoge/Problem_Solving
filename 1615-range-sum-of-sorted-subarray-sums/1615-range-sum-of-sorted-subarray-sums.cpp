4eeb4b4a5c3018c68e02e8e178d925ee6cd1c77b
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-04 11:19:46 +0900

    Time: 98 ms (28.31%), Space: 19.2 MB (72.81%) - LeetHub

--- 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/1615-range-sum-of-sorted-subarray-sums.cpp
@@ -20,19 +20,19 @@ public:
         vector<int> newSums;
         newSums.reserve(n * (n + 1) / 2);
         
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j <= n; ++j) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
                 newSums.push_back(prefix[j] - prefix[i]);
             }
         }
         
         sort(newSums.begin(), newSums.end());
         
-        int ans = 0;
+        long long ans = 0;
         for (int i = left - 1; i < right; i++) {
-            ans = (ans + newSums[i]) % 1000000007;
+            ans += newSums[i];
         }
         
-        return ans;
+        return ans % (int)(1e9 + 7);
     }
 };
