08ea3b8046d2f11bc46fa76b78368f4bca4077f3
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-07 10:40:36 +0900

    Time: 4 ms (49.19%), Space: 12.2 MB (44.7%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -0,0 +1,51 @@
+class Solution {
+private:
+    vector<string> numWords = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> tys = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+public:
+    string convert(int digit, vector<int>& nums) {
+        string res = "";
+
+        // 100
+        if(nums[digit+2] != 0) {
+            res += (numWords[nums[digit+2]] + " Hundred ");
+        }
+
+        // 0 ~ 99
+        if(nums[digit+1] == 0) {
+            res += numWords[nums[digit]];
+            if(nums[digit]) res += " ";
+        } else if(nums[digit+1] == 1) {
+            res += teens[nums[digit]];
+            res += " ";
+        } else {
+            res += tys[nums[digit+1]];
+            if(nums[digit]) {
+                res += " ";
+                res += numWords[nums[digit]];
+            }
+            res += " ";
+        }
+
+        return res;
+    }
+    string numberToWords(int num) {
+        if(num == 0) return "Zero";
+        vector<int> nums;
+        while(num > 0) {
+            nums.push_back(num % 10);
+            num /= 10;
+        }
+        while(nums.size() < 10) {
+            nums.push_back(0);
+        }
+        string n = convert(6, nums);
+        string m = convert(3, nums);
+
+        string ans = (nums[9]? numWords[nums[9]] + " Billion ":"") + (n!=""? n + "Million ":"") + (m!=""? m + "Thousand ":"") + convert(0, nums);
+
+        ans.pop_back();
+        return ans;
+    }
+};
