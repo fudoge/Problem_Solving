478d8835dd8e52da87496aaa9804f3c0c140eb24
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-11 12:23:39 +0900

    Time: 136 ms (20.98%), Space: 36.9 MB (39.08%) - LeetHub

--- 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.cpp
+++ 1691-minimum-number-of-days-to-disconnect-island/1691-minimum-number-of-days-to-disconnect-island.cpp
@@ -1,3 +1,11 @@
+#pragma GCC optimize("03", "unroll-loops");
+static const int __ = [](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
 class Solution {
 private:
     vector<int> dx = {1, -1, 0, 0};
