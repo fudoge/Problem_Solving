cad6be1ba7bd39930ed0ac43f93335b69a0db841
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-12 16:13:01 +0900

    Time: 24 ms (73.5%), Space: 24.6 MB (48.25%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -1,23 +1,20 @@
 class KthLargest {
 private:
     int k;
-    vector<int> arr;
+    priority_queue<int, vector<int>, greater<int>> pq;
 public:
     KthLargest(int k, vector<int>& nums) {
         this -> k = k;
-        sort(nums.begin(), nums.end(), greater<int>());
-        for(const auto &num : nums) arr.push_back(num);
+        for(int element : nums) {
+            pq.push(element);
+            if(pq.size() > k) pq.pop();
+        }
     }
     
     int add(int val) {
-        for(int i = 0; i < arr.size(); i++) {
-            if(val > arr[i]) {
-                arr.insert(arr.begin() + i, val);
-                return arr[k-1];
-            }
-        }
-        arr.push_back(val);
-        return arr[k-1];
+        pq.push(val);
+        if(pq.size() > k) pq.pop();
+        return pq.top();
     }
 };
 
