3c372a09eabc45c3ec4e0db1bd8f78c15396a251
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-16 10:38:49 +0900

    [level 3] Title: 최고의 집합, Time: 2.19 ms, Memory: 5.49 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.cpp
@@ -1,26 +1,14 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
 vector<int> solution(int n, int s) {
-    if(s < 2) return {-1};
-    if(n > s) return {-1};
-    if(n == 1) return {s};
-    if(n == s) {
-        return vector<int>(n, 1);
+    if(s < n) return {-1};
+
+    vector<int> answer(n, s / n);
+    for(int i = 0; i < s % n; ++i) {
+        answer[n-1-i]++;
     }
-    
-    vector<int> answer;
-    int quotient = s / n;
-    int remainder = s % n;
-    for(int i = 0; i < n; ++i) {
-        answer.push_back(quotient);
-    }
-    for(int i = n-1; i >= 0 && remainder > 0; --i) {
-        answer[i]++;
-        remainder--;
-    }
-    if(s == 1) answer[answer.size()-1]++;
+
     return answer;
 }
