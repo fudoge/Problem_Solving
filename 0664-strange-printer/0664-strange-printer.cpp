ae40ebe6687656364191ea2e71ed46b63249765f
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-21 13:00:22 +0900

    Time: 21 ms (57.45%), Space: 11.2 MB (37.94%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,3 +1,11 @@
+#pragma GCC optimize("03", "unroll-loops");
+static const int __ = [](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
 class Solution {
 private:
     vector<vector<int>> dp;
@@ -7,9 +15,9 @@ public:
         if (dp[from][to] != -1) return dp[from][to];
 
         dp[from][to] = solve(from + 1, to, s) + 1;
-        for (int k = from + 1; k <= to; ++k) {
-            if (s[from] == s[k]) {
-                dp[from][to] = min(dp[from][to], solve(from, k - 1, s) + solve(k + 1, to, s));
+        for (int i = from + 1; i <= to; ++i) {
+            if (s[from] == s[i]) {
+                dp[from][to] = min(dp[from][to], solve(from, i - 1, s) + solve(i + 1, to, s));
             }
         }
         return dp[from][to];
