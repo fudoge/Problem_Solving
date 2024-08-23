78b429671a10dd1c8b4c77ca052893b0d2377173
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-23 13:12:29 +0900

    Time: 0 ms (100%), Space: 7.7 MB (58.91%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.cpp
@@ -1,65 +1,45 @@
 class Solution {
 public:
     int gcd(int a, int b) {
-        if(b == 0) {
-            return a;
-        }
-        return gcd(b, a%b);
+        return b == 0 ? a : gcd(b, a % b);
     }
 
-    int get_lcm(int a, int b) {
-        return a * b / gcd(max(a, b), min(a, b));
+    int lcm(int a, int b) {
+        return a * (b / gcd(a, b));
     }
 
     string fractionAddition(string expression) {
-        vector<pair<int, int>> fracs;
-        for(int i = 0; i < expression.size(); i++) {
-            bool isPositive = true;
+        int curr_numerator = 0, curr_denominator = 1;
+        int i = 0, n = expression.size();
+
+        while (i < n) {
+            int sign = 1;
+            if (expression[i] == '-' || expression[i] == '+') {
+                sign = (expression[i++] == '-') ? -1 : 1;
+            }
+
             int numerator = 0;
-            int denominator = 0;
-            if(expression[i] == '-') {
-                isPositive = false;
-            } else if(i == 0) i--;
-            i++;
-            
-            while(isdigit(expression[i])) {
+            while (isdigit(expression[i])) {
                 numerator = numerator * 10 + (expression[i++] - '0');
             }
-            ++i;
+            numerator *= sign;
 
-            while(isdigit(expression[i])) {
+            i++;
+
+            int denominator = 0;
+            while (i < n && isdigit(expression[i])) {
                 denominator = denominator * 10 + (expression[i++] - '0');
             }
-            --i;
 
-            if(!isPositive) {
-                numerator *= -1;
-            }
-
-            fracs.push_back({numerator, denominator});
+            int lcm_val = lcm(curr_denominator, denominator);
+            curr_numerator = curr_numerator * (lcm_val / curr_denominator) + numerator * (lcm_val / denominator);
+            curr_denominator = lcm_val;
         }
 
-        int curr_numerator = 0;
-        int curr_denominator = 1;
-
-        for(auto &f : fracs) {
-            int lcm = get_lcm(curr_denominator, f.second);
-
-            curr_numerator *= lcm / curr_denominator;
-            f.first *= lcm / f.second;
-
-            curr_numerator += f.first;
-            curr_denominator = lcm;
-        }
-
-        if(curr_numerator == 0) return "0/1";
-        int g = gcd(max(curr_numerator, curr_denominator), min(curr_numerator, curr_denominator));
+        int g = gcd(abs(curr_numerator), curr_denominator);
         curr_numerator /= g;
         curr_denominator /= g;
-        if(curr_denominator < 0) {
-            curr_numerator *= -1;
-            curr_denominator *= -1;
-        }
+
         return to_string(curr_numerator) + '/' + to_string(curr_denominator);
     }
 };
