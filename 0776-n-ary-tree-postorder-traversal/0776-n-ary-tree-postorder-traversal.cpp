d445ade945a854695743fe93f7aeeca1ca365cc2
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-26 09:47:02 +0900

    Time: 11 ms (78.54%), Space: 15.3 MB (49.47%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -18,14 +18,6 @@ public:
 };
 */
 
-#pragma GCC optimize("03", "unroll-loops");
-static const int __ = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
-
 class Solution {
 private:
     void traverse(Node* root, vector<int>& ans) {
