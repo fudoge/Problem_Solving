d68d3d231349a084933730448207f9019c4ad9f5
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-26 10:08:11 +0900

    Time: 0 ms (100%), Space: 4.2 MB (87.63%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.go
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.go
@@ -5,18 +5,30 @@
  *     Children []*Node
  * }
  */
-func traverse(root *Node, ans *[]int) {
-    if root == nil {
-        return
-    }
-    for _, child := range root.Children {
-        traverse(child, ans)
-    }
-    *ans = append(*ans, root.Val)
-}
 
 func postorder(root *Node) []int {
+    if root == nil {
+        return nil
+    }
+    
+    var stack []*Node
     var ans []int
-    traverse(root, &ans)
+
+    stack = append(stack, root)
+    for len(stack) > 0 {
+        curr := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        
+        ans = append(ans, curr.Val)
+        
+        for _, child := range curr.Children {
+            stack = append(stack, child)
+        }
+    }
+    
+    for i, j := 0, len(ans)-1; i < j; i, j = i+1, j-1 {
+        ans[i], ans[j] = ans[j], ans[i]
+    }
+    
     return ans
 }
