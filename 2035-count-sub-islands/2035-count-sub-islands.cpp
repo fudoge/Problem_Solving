588d3e03396e8e897ffc47be673ebc2d4c591d8f
fudoge <77766067+fudoge@users.noreply.github.com>
2024-08-28 09:52:23 +0900

    Time: 167 ms (98.64%), Space: 96.2 MB (55.33%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.cpp
+++ 2035-count-sub-islands/2035-count-sub-islands.cpp
@@ -10,30 +10,15 @@ class Solution {
 private:
     int m;
     int n;
-    vector<int> dx;
-    vector<int> dy;
-    bool bfs(int r, int c, vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        bool flag = true;
-        queue<pair<int, int>> q;
-        q.push({r, c});
+    bool dfs(int r, int c, vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        if(r < 0 || r >= m || c < 0 || c >= n || grid2[r][c] == 0) return true;
+        bool flag = grid1[r][c] == 1;
+        grid2[r][c] = 0;
+        flag &= dfs(r-1, c, grid1, grid2);
+        flag &= dfs(r+1, c, grid1, grid2);
+        flag &= dfs(r, c-1, grid1, grid2);
+        flag &= dfs(r, c+1, grid1, grid2);
 
-        while(!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-
-            for(int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-                if(grid2[nx][ny]) {
-                    if(!grid1[nx][ny]) flag = false;
-                    grid2[nx][ny] = 0;
-                    q.push({nx, ny});
-                }
-            }
-        }
 
         return flag;
     }
@@ -41,14 +26,11 @@ public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         m = grid1.size();
         n = grid1[0].size();
-        dx = {-1, 1, 0, 0};
-        dy = {0, 0, -1, 1};
-        
         int ans = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(grid1[i][j] && grid2[i][j]) {
-                    if(bfs(i, j, grid1, grid2)) ans++;
+                    if(dfs(i, j, grid1, grid2)) ans++;
                 }
             }
         }
