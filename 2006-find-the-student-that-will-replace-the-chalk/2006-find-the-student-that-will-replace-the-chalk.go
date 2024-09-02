985060bd2efdd29735ff68e97497b27e26c847e0
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-02 09:18:48 +0900

    Time: 85 ms (96.55%), Space: 8.2 MB (79.31%) - LeetHub

---
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.go
@@ -0,0 +1,17 @@
+func chalkReplacer(chalk []int, k int) int {
+    var total int64
+
+    for _, c := range chalk {
+        total += int64(c)
+    }
+    k = int(int64(k) % total)
+
+    for i, v := range chalk {
+        if v <= k {
+            k -= v
+        } else {
+            return i
+        }
+    }
+    return 0;
+}
