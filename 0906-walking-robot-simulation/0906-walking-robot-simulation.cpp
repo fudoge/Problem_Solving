01de1059a4a570087fefe8e2d53162adad61f846
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-04 10:32:51 +0900

    Time: 124 ms (23.2%), Space: 39.6 MB (59.67%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -1,111 +1,35 @@
-#pragma GCC optimize("03", "unroll-loops");
-static const int __ = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
-
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        vector<char> directions = {'n', 'w', 's', 'e'}; // 0: north, 1: west, 2: south, 3: east
-        pair<int, int> pos;
-        pos = {0, 0};
-        char direction = 'n';
-        int dircode = 0;
+        // n, w, s, e
+        vector<pair<int, int>> directions = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+        int dircode = 0; 
+        pair<int, int> pos = {0, 0};
         int ans = 0;
 
-        unordered_map<int, vector<int>> xAxisObs;
-        unordered_map<int, vector<int>> yAxisObs;
-
-        for(const auto &obstacle : obstacles) {
-            int x = obstacle[0];
-            int y = obstacle[1];
-
-            xAxisObs[x].push_back(y);
-            yAxisObs[y].push_back(x);
-        }
-
-        for(auto &element : xAxisObs) {
-            sort(element.second.begin(), element.second.end());
-        }
-
-        for(auto &element : yAxisObs) {
-            sort(element.second.begin(), element.second.end());
+        unordered_set<string> obstacleSet;
+        for(const auto& obs : obstacles) {
+            obstacleSet.insert(to_string(obs[0]) + "," + to_string(obs[1]));
         }
 
         for(int command : commands) {
             if(command == -2) {
-                direction = directions[(dircode = (dircode +1 +4) % 4)];
+                dircode = (dircode + 1) % 4;
             } else if(command == -1) {
-                direction = directions[(dircode = (dircode -1 +4) % 4)];
+                dircode = (dircode + 3) % 4;
             } else {
-                int k = command;
-                bool flag = true;
-                switch (direction) {
-                    case 'n': {
-                        int xPos = pos.first;
-                        int yPos = pos.second;
-                        int yGoal = yPos + k;
-                        for(int candidatedObs : xAxisObs[xPos]) {
-                            if(candidatedObs > yPos && candidatedObs <= yGoal) {
-                                flag = false;
-                                yPos = candidatedObs-1;
-                                break;
-                            }
-                        }
-                        if(flag) yPos = yGoal;
-                        pos.second = yPos;
-                        break;
-                    }
-                    case 'w': {
-                        int xPos = pos.first;
-                        int yPos = pos.second;
-                        int xGoal = xPos - k;
-                        for(int candidatedObs : yAxisObs[yPos]) {
-                            if(candidatedObs < xPos && candidatedObs >= xGoal) {
-                                flag = false;
-                                xPos = candidatedObs+1;
-                                break;
-                            }
-                        }
-                        if(flag) xPos = xGoal;
-                        pos.first = xPos;
-                        break;
-                    }
-                    case 's': {
-                        int xPos = pos.first;
-                        int yPos = pos.second;
-                        int yGoal = yPos - k;
-                        for(int candidatedObs : xAxisObs[xPos]) {
-                            if(candidatedObs < yPos && candidatedObs >= yGoal) {
-                                flag = false;
-                                yPos = candidatedObs+1;
-                                break;
-                            }
-                        }
-                        if(flag) yPos = yGoal;
-                        pos.second = yPos;
-                        break;
-                    }
-                    case 'e': {
-                        int xPos = pos.first;
-                        int yPos = pos.second;
-                        int xGoal = xPos + k;
-                        for(int candidatedObs : yAxisObs[yPos]) {
-                            if(candidatedObs > xPos && candidatedObs <= xGoal) {
-                                flag = false;
-                                xPos = candidatedObs-1;
-                                break;
-                            }
-                        }
-                        if(flag) xPos = xGoal;
-                        pos.first = xPos;
-                        break;
-                    }
+                for(int i = 0; i < command; ++i) {
+                    int nextX = pos.first + directions[dircode].first;
+                    int nextY = pos.second + directions[dircode].second;
+                    string nextPos = to_string(nextX) + "," + to_string(nextY);
+
+                    if(obstacleSet.count(nextPos)) break;
+
+                    pos.first = nextX;
+                    pos.second = nextY;
+
+                    ans = max(ans, pos.first * pos.first + pos.second * pos.second);
                 }
-                ans = max(ans, pos.first*pos.first + pos.second*pos.second);
             }
         }
 
