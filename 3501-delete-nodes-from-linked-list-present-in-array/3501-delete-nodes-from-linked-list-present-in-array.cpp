4b308805f9c231829c5426cdbe1d91fb272de739
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-06 09:33:52 +0900

    Time: 391 ms (96.22%), Space: 261 MB (57.68%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -20,28 +20,24 @@ static const int __ = [](){
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        auto curr = head;
         set<int> s(nums.begin(), nums.end());
-        ListNode* newHead = NULL;
-        ListNode* newTail = NULL;
+        while (head != nullptr && s.find(head->val) != s.end()) {
+            head = head->next;
+        }
 
-        while(curr != NULL) {
-            if(s.find(curr -> val) == s.end()) {
-                if(!newHead) {
-                    newHead = curr;
-                    newTail = curr;
-                } else {
-                    newTail -> next = curr;
-                    newTail = newTail -> next;
-                }
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* curr = head;
+
+        while (curr != nullptr && curr->next != nullptr) {
+            if (s.find(curr->next->val) != s.end()) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
             }
-            curr = curr -> next;
         }
 
-        if(newTail != NULL) {
-            newTail -> next = NULL;
-        }
-
-        return newHead;
+        return head;
     }
 };
