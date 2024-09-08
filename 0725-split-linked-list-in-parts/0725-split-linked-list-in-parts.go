e9e0e4b1f4d68b38f0737340b45ddcb2dbeb9135
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-08 11:49:17 +0900

    Time: 2 ms (77.08%), Space: 2.9 MB (20.83%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.go
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func splitListToParts(head *ListNode, k int) []*ListNode {
+    n := 0
+    curr := head
+
+    for curr != nil {
+        n++
+        curr = curr.Next
+    }
+
+    rest := n % k
+    div := n / k
+    var prev *ListNode
+    curr = head
+    ans := make([]*ListNode,0)
+
+    for i := 0; i < rest; i++ {
+        ans = append(ans, curr)
+        for j := 0; j < div+1; j++ {
+            prev = curr
+            curr = curr.Next
+        }
+        prev.Next = nil
+    }
+
+    for curr != nil {
+        ans = append(ans, curr)
+        for j := 0; j < div; j++ {
+            prev = curr
+            curr = curr.Next
+        }
+        prev.Next = nil
+    }
+
+    for i := len(ans); i < k; i++ {
+        ans = append(ans, nil)
+    }
+
+    return ans
+}
