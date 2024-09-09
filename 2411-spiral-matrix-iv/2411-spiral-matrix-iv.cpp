b8354b3f4f8999683f644d19cd24774ba3eb8f79
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-09 09:47:36 +0900

    Time: 167 ms (50.07%), Space: 130.5 MB (65.45%) - LeetHub

---
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> ans(m, vector<int>(n, -1));
+        int rowStart = 0, rowEnd = m - 1;
+        int colStart = 0, colEnd = n - 1;
+        
+        while (head && rowStart <= rowEnd && colStart <= colEnd) {
+            for (int col = colStart; col <= colEnd && head; ++col) {
+                ans[rowStart][col] = head->val;
+                head = head->next;
+            }
+            rowStart++;
+            
+            for (int row = rowStart; row <= rowEnd && head; ++row) {
+                ans[row][colEnd] = head->val;
+                head = head->next;
+            }
+            colEnd--;
+            
+            for (int col = colEnd; col >= colStart && head; --col) {
+                ans[rowEnd][col] = head->val;
+                head = head->next;
+            }
+            rowEnd--;
+            
+            for (int row = rowEnd; row >= rowStart && head; --row) {
+                ans[row][colStart] = head->val;
+                head = head->next;
+            }
+            colStart++;
+        }
+        
+        return ans;
+    }
+};
