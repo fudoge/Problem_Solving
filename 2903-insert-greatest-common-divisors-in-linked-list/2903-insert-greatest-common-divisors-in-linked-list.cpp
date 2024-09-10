286939ad0059a077971c6e81be52433862d1150c
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-10 11:27:03 +0900

    Time: 38 ms (64.33%), Space: 35.6 MB (49.64%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
@@ -9,14 +9,6 @@
  * };
  */
 
-#pragma GCC optimize("03", "unroll-loops");
-static const int __ = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
-
 class Solution {
 private:
     int gcd(int a, int b) {
