58f5b6ea6c6bdabe80cfdf010891e9b268fb4cbb
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-17 11:58:37 +0900

    Time: 0 ms (100%), Space: 8.9 MB (72.35%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        unordered_map<string, int> mp1;
-        unordered_map<string, int> mp2;
+        unordered_map<string, int> mp;
 
         stringstream ss1(s1);
         stringstream ss2(s2);
@@ -10,19 +9,15 @@ public:
         string token;
         vector<string> ans;
         while(ss1 >> token) {
-            mp1[token]++;
+            mp[token]++;
         }
 
         while(ss2 >> token) {
-            mp2[token]++;
+            mp[token]++;
         }
 
-        for(auto &element : mp1) {
-            if(element.second == 1 &&  mp2[element.first] < 1) ans.push_back({element.first});
-        }
-
-        for(auto &element : mp2) {
-            if(element.second == 1 &&  mp1[element.first] < 1) ans.push_back({element.first});
+        for(auto &element : mp) {
+            if(element.second == 1) ans.push_back(element.first);
         }
 
         return ans;
