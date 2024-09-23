8532143b9271925e208729173ad36cc1280dc6b6
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-23 09:21:58 +0900

    Time: 116 ms (59.63%), Space: 80.1 MB (61.49%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,39 +1,28 @@
 class Solution {
-public:
-    int minExtraChar(string s, vector<string> dictionary) {
-        unordered_map<int, int> cache;
-
-        int result = dfs(s, dictionary, cache);
-        return result;
-    }
-
 private:
-    int dfs(const string& text, const vector<string>& dictionary, unordered_map<int, int>& cache) {
-        if (text.empty()) {
-            return 0;
-        }
+    int dfs(const string &suffix, vector<string> &dictionary, unordered_map<int, int> &cache) {
+        int len = suffix.size();
+        if(len == 0) return 0;
 
-        if (cache.find(text.length()) != cache.end()) {
-            return cache[text.length()];
-        }
+        if(cache.find(len) != cache.end()) return cache[len];
 
-        int minimum = text.length();
-
-        for (const string& word : dictionary) {
-            if (text == word) {
-                minimum = 0;
-                cache[text.length()] = minimum;
-                return minimum;
-            } else if (text.substr(0, word.length()) == word) {
-                int result = dfs(text.substr(word.length()), dictionary, cache);
-                minimum = min(minimum, result);
+        int minimum = len;
+        for(const auto &word : dictionary) {
+            if(suffix.substr(0, word.size()) == word){
+                minimum = min(minimum, dfs(suffix.substr(word.size()), dictionary, cache));
             }
         }
 
-        int result = 1 + dfs(text.substr(1), dictionary, cache);
-        minimum = min(minimum, result);
+        minimum = min(minimum, 1 + dfs(suffix.substr(1), dictionary, cache));
 
-        cache[text.length()] = minimum;
+        cache[len] = minimum;
         return minimum;
+    }
+
+public:
+    int minExtraChar(string s, vector<string>& dictionary) {
+        unordered_map<int, int> cache;
+
+        return dfs(s, dictionary, cache);
     }
 };
