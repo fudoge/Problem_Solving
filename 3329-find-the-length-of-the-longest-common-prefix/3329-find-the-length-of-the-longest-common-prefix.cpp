d9c88d6e8c4e75372d7bca88dd3fe97accd1ff17
fudoge <77766067+fudoge@users.noreply.github.com>
2024-09-24 13:55:02 +0900

    Time: 140 ms (99.53%), Space: 167.6 MB (49.18%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,31 +1,78 @@
-#pragma GCC optimize("03", "unroll-loops");
-static const int __ = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
+class TrieNode {
+public:
+    int data;
+    bool isTerminal;
+    TrieNode* children[10];
+
+    TrieNode(int ch) {
+        data = ch;
+        isTerminal = false;
+        for(int i = 0; i < 10; ++i) {
+            children[i] = NULL;
+        }
+    }
+};
+
+class Trie {
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode(-1);
+    }
+
+    void insert(int data) {
+        auto node = root;
+
+        int divisor = 1;
+        while (data / divisor >= 10) {
+            divisor *= 10;
+        }
+
+        while (divisor > 0) {
+            int digit = (data / divisor) % 10;
+            if (node->children[digit] == NULL) {
+                node->children[digit] = new TrieNode(digit);
+            }
+            node = node->children[digit];
+            divisor /= 10;
+        }
+        node->isTerminal = true;
+    }
+
+    int prefixLen(int data) {
+        auto node = root;
+        int len = 0;
+        int divisor = 1;
+        while (data / divisor >= 10) {
+            divisor *= 10;
+        }
+
+        while (divisor > 0) {
+            int digit = (data / divisor) % 10;
+            if (node->children[digit] == nullptr) {
+                return len;
+            } else {
+                len++;
+                node = node->children[digit];
+                divisor /= 10;
+            }
+        }
+
+        return len;
+    }
+};
 
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<int, bool> prefixes;
+        Trie* t = new Trie();
         for(int num : arr2) {
-            while(num > 0) {
-                prefixes[num] = true;
-                num /= 10;
-            }
+            t -> insert(num);
         }
-        
+
         int ans = 0;
-        for(auto &num : arr1) {
-            while(num > 0) {
-                if(prefixes[num]) {
-                    ans = max(ans, (int)log10(num) + 1);
-                    break;
-                }
-                num /= 10;
-            }
+        for(int num : arr1) {
+            ans = max(ans, t -> prefixLen(num));
         }
 
         return ans;
