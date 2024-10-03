a290f4c2a556795aa53632b584e0caf26756e062
fudoge <77766067+fudoge@users.noreply.github.com>
2024-10-03 13:20:23 +0900

    Time: 141 ms (37.28%), Space: 74.5 MB (12.34%) - LeetHub

---
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        long long sum = 0;
+        for(auto &num : nums) sum += num;
+        long long remainder = sum % p;
+        
+        if(remainder == 0) return 0;
+        
+        unordered_map<long, int> prefix;
+        prefix[0] = -1;
+        long long current = 0;
+        int min_len = nums.size();
+        
+        for(int i = 0; i < nums.size(); ++i){
+            current = (current + nums[i]) % p;
+            long long target = (current - remainder + p) % p;
+            if(prefix.find(target) != prefix.end()){
+                min_len = min(min_len, i - prefix[target]);
+            }
+            prefix[current] = i;
+        }
+        
+        return min_len < nums.size() ? min_len : -1;
+    }
+};
