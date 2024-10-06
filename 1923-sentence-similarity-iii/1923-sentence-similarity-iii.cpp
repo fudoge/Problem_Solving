d1290432d1d92db3c3025f252d9999325b15b7e1
fudoge <77766067+fudoge@users.noreply.github.com>
2024-10-06 14:59:32 +0900

    Time: 2 ms (56.68%), Space: 8.8 MB (22.58%) - LeetHub

---
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<string> split(string &sentence) {
+        stringstream ss(sentence);
+        string word;
+        vector<string> arr;
+        while(ss >> word) {
+            arr.push_back(word);
+        }
+        return arr;
+    }
+public:
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        auto arr1 = split(sentence1);
+        auto arr2 = split(sentence2);
+
+        if(arr1.size() > arr2.size()) swap(arr1, arr2);
+
+        int n = arr1.size();
+        int m = arr2.size();
+
+        int left = 0, right = n-1;
+        while(left < n && arr1[left] == arr2[left]) left++;
+        while(right >= 0 && arr1[right] == arr2[right + m - n]) right--;
+
+        return left > right;
+    }
+};
