d44cb183aacb6b41064d39bf9706314fdde43cd6
fudoge <77766067+fudoge@users.noreply.github.com>
2024-10-13 17:27:35 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 264 ms, Memory: 62428 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int n, m;
+
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
 int bfs(vector<vector<int>> &mat) {
-  vector<vector<vector<int>>> minDistance(n, vector<vector<int>>(m, vector<int>(2, INT_MAX)));
-  queue<tuple<int, int, int>> q;
-  
-  q.emplace(0, 0, 0);
+  vector<vector<vector<int>>> minDistance(
+      n, vector<vector<int>>(m, vector<int>(2, INT_MAX)));
   minDistance[0][0][0] = 1;
+  queue<tuple<int, int, int>> q;
+  q.push({0, 0, 0});
 
   while (!q.empty()) {
     int x, y, broken;
@@ -26,8 +28,8 @@ int bfs(vector<vector<int>> &mat) {
 
       if (nx < 0 || ny < 0 || nx >= n || ny >= m)
         continue;
-
-      if (mat[nx][ny] == 1 && broken == 0 && minDistance[nx][ny][1] == INT_MAX) {
+      if (mat[nx][ny] == 1 && broken == 0 &&
+          minDistance[nx][ny][1] == INT_MAX) {
         minDistance[nx][ny][1] = minDistance[x][y][0] + 1;
         q.emplace(nx, ny, 1);
       } else if (mat[nx][ny] == 0 && minDistance[nx][ny][broken] == INT_MAX) {
@@ -40,13 +42,14 @@ int bfs(vector<vector<int>> &mat) {
   return -1;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
 
   cin >> n >> m;
   vector<vector<int>> mat(n, vector<int>(m));
+
   string temp;
   for (int i = 0; i < n; ++i) {
     cin >> temp;
