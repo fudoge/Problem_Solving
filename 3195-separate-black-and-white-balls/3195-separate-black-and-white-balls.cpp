f448280a0594dd1f3a1c85c316c1c1eac77e2c04
fudoge <77766067+fudoge@users.noreply.github.com>
2024-10-15 11:03:36 +0900

    Time: 15 ms (98.95%), Space: 15.8 MB (10.74%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,3 +1,11 @@
+#pragma GCC optimize("03", "unroll-loops");
+static const int __ = [](){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
 class Solution {
 public:
     long long minimumSteps(string s) {
