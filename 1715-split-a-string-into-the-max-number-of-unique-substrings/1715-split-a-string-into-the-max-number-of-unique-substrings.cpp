d49f6bf050fd0823792769a936e388520abd2844
fudoge <77766067+fudoge@users.noreply.github.com>
2024-10-21 09:20:44 +0900

    Time: 68 ms (92.4%), Space: 9.9 MB (99.42%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -10,7 +10,6 @@ private:
 
         string temp = string(1, s[idx]);
         for(int i = idx+1; i <= n; ++i) {
-            cout << i << " " << temp << endl;
             if(exists[temp] == 0) {
                 exists[temp]++;
                 solve(i, count+1, s);
