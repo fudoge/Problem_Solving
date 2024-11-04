93e53190677c037451db7b34b5e7e32c5f8053b3
fudoge <77766067+fudoge@users.noreply.github.com>
2024-11-04 10:04:13 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -2,44 +2,27 @@
 
 using namespace std;
 
-int n, k;
-
-int dp[101][100001];
-vector<pair<int, int>> items;
-
-int solve(int idx, int leftWeight) {
-  if (idx >= n)
-    return 0;
-  if (dp[idx][leftWeight] != -1)
-    return dp[idx][leftWeight];
-
-  int res = 0;
-  if (items[idx].first <= leftWeight) {
-    res = solve(idx + 1, leftWeight - items[idx].first) + items[idx].second;
-  }
-  res = max(res, solve(idx + 1, leftWeight));
-
-  return dp[idx][leftWeight] = res;
-}
-
 int main(int argc, char *argv[]) {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
-
+  int n, k;
   cin >> n >> k;
-  items.resize(n);
-  memset(dp, -1, sizeof(dp));
 
   int w, v;
+  vector<pair<int, int>> items(n);
   for (int i = 0; i < n; ++i) {
     cin >> w >> v;
-    items[i].first = w;
-    items[i].second = v;
+    items[i] = {w, v};
   }
-  sort(items.begin(), items.end());
 
-  cout << solve(0, k) << "\n";
+  vector<int> dp(k + 1, 0);
+  for (int i = 0; i < n; ++i) {
+    w = items[i].first;
+    v = items[i].second;
+    for (int j = k; j >= w; --j) {
+      dp[j] = max(dp[j], dp[j - w] + v);
+    }
+  }
+
+  cout << dp[k] << "\n";
 
   return 0;
 }
