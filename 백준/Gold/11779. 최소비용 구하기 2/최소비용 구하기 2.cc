07c10760d7deca496fc5a7d8b12e0a716e6c334f
fudoge <77766067+fudoge@users.noreply.github.com>
2024-11-25 09:45:21 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 120 ms, Memory: 6400 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+
+int n, m;
+int src, dst;
+
+struct comp {
+  bool operator()(vector<int> &a, vector<int> &b) {
+    if (a[0] == b[0])
+      return a.size() > b.size();
+    return a[0] < b[0];
+  }
+};
+
+int main(int argc, char *argv[]) {
+  cin >> n >> m;
+  vector<vector<pair<int, int>>> graph(n + 1);
+
+  int u, v, w;
+  for (int i = 0; i < m; ++i) {
+    cin >> u >> v >> w;
+    graph[u].push_back({v, w});
+  }
+  cin >> src >> dst;
+
+  vector<vector<int>> dist(n + 1, {(int)1e9}); // {비용, 경로}
+  dist[src] = {0, src};
+
+  priority_queue<vector<int>, vector<vector<int>>, comp> pq; // <비용, 경로>
+  pq.push({0, src});
+
+  while (!pq.empty()) {
+    auto currState = pq.top();
+    pq.pop();
+
+    int stateSize = currState.size();
+    int currNode = currState[stateSize - 1];
+    int currCost = currState[0];
+
+    if (dist[currNode][0] < currCost)
+      continue;
+
+    for (const auto &next : graph[currNode]) {
+      int nextCost = currState[0] + next.second;
+      if (nextCost < dist[next.first][0]) {
+        vector<int> nextState(stateSize + 1);
+        nextState[0] = nextCost;
+        for (int i = 1; i < stateSize; ++i) {
+          nextState[i] = currState[i];
+        }
+        nextState[stateSize] = next.first;
+        dist[next.first] = nextState;
+        pq.push(nextState);
+      }
+    }
+  }
+
+  cout << dist[dst][0] << "\n";
+  cout << dist[dst].size() - 1 << "\n";
+  for (int i = 1; i < dist[dst].size(); ++i) {
+    cout << dist[dst][i] << " ";
+  }
+  cout << "\n";
+
+  return 0;
+}
