a65493c862d5cfaae30b482e0724be71e2cdf2c4
fudoge <77766067+fudoge@users.noreply.github.com>
2025-02-03 19:34:59 +0900

    [Silver III] Title: N과 M (1), Time: 44 ms, Memory: 924 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var n int32
+var m int32
+var reader *bufio.Reader
+var writer *bufio.Writer
+
+func solve(visited []bool, arr []int32) {
+	if int32(len(arr)) == m {
+		for _, v := range arr {
+			fmt.Fprint(writer, v)
+			fmt.Fprint(writer, " ")
+		}
+		fmt.Fprintln(writer)
+		return
+	}
+
+	for i := int32(1); i <= n; i++ {
+		if visited[i] == false {
+			visited[i] = true
+			arr = append(arr, i)
+			solve(visited, arr)
+			visited[i] = false
+			arr = arr[:len(arr)-1]
+		}
+	}
+}
+
+func main() {
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	fmt.Fscan(reader, &n)
+	fmt.Fscan(reader, &m)
+
+	arr := make([]int32, 0)
+	visited := make([]bool, n+1)
+	for i := int32(1); i <= n; i++ {
+		visited[i] = true
+		arr = append(arr, i)
+		solve(visited, arr)
+		visited[i] = false
+		arr = arr[:len(arr)-1]
+	}
+}
