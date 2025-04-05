1839f0fb378893a8f0256abe522339adf037b648
fudoge <77766067+fudoge@users.noreply.github.com>
2025-04-05 13:34:40 +0900

    Time: 0 ms (100%), Space: 9.2 MB (71.99%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.cpp
@@ -1,11 +1,11 @@
 class Solution {
-public:
+   public:
     int subsetXORSum(vector<int>& nums) {
-        int xor_sum = 0;
-        for (int num : nums) {
-            xor_sum |= num;  // 모든 숫자의 비트를 OR 연산합니다.
+        int ans = 0;
+        for(const auto & num : nums) {
+            ans |= num;
         }
-        int n = nums.size();
-        return xor_sum * (1 << (n - 1)); // 2^(n-1)을 곱합니다.
+
+        return ans * (1 << (nums.size()-1));
     }
 };
