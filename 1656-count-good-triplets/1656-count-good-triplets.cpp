4cb72744039f38f4b63b25afb194dd60bf4f8a07
fudoge <77766067+fudoge@users.noreply.github.com>
2025-04-14 09:25:49 +0900

    Time: 9 ms (41.98%), Space: 11.5 MB (5.91%) - LeetHub

---
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -0,0 +1,23 @@
+#pragma GCC optimize("O3", "unroll-loops");
+auto init = []() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
+class Solution {
+   public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int n = arr.size();
+        int ans = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                if (abs(arr[i] - arr[j]) > a) continue;
+                for (int k = j + 1; k < n; k++) {
+                    if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
+                        ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
