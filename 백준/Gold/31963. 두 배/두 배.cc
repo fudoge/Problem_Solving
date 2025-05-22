2197a9211b363671ef4a14237c31978ae654b0f0
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-05-22 14:36:31 +0900

    [Gold III] Title: 두 배, Time: 36 ms, Memory: 9844 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.cc
+++ 백준/Gold/31963. 두 배/두 배.cc
@@ -1,51 +1,48 @@
 #include <bits/stdc++.h>
 
-#include <numeric>
-
 using namespace std;
-
-int LIMIT = 1 << 20;
+using ll = long long;
 
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n;
     cin >> n;
-    vector<int> arr(n), baselen(n), exps(n), more_exps(n);
+    vector<ll> arr(n), baselen(n), exps(n), more_exps(n);
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
         while (arr[i] % 2 == 0) {
             arr[i] >>= 1;
             exps[i]++;
         }
-        baselen[i] = ceil(log2(arr[i] + 1));
+        baselen[i] = 63 - __builtin_clz(arr[i]) + 1;
     }
 
     for (int i = 1; i < n; i++) {
-        int prevbaselen = baselen[i - 1];
-        int prevbase = arr[i - 1];
-        int prevexp = exps[i - 1] + more_exps[i - 1];
+        ll prevbaselen = baselen[i - 1];
+        ll prevbase = arr[i - 1];
+        ll prevexp = exps[i - 1] + more_exps[i - 1];
 
-        int curbaselen = baselen[i];
-        int curbase = arr[i];
-        int curexp = exps[i];
+        ll curbaselen = baselen[i];
+        ll curbase = arr[i];
+        ll curexp = exps[i];
 
         if (prevbaselen < curbaselen) {
-            int lendiff = curbaselen - prevbaselen;
+            ll lendiff = curbaselen - prevbaselen;
             prevbase <<= lendiff;
             prevexp -= lendiff;
         } else if (curbaselen < prevbaselen) {
-            int lendiff = prevbaselen - curbaselen;
+            ll lendiff = prevbaselen - curbaselen;
             curbase <<= lendiff;
             curexp -= lendiff;
         }
 
-        int expdiff = prevexp - curexp;
+        ll expdiff = prevexp - curexp;
         if (expdiff > 0) more_exps[i] = expdiff;
         if (curexp <= prevexp && curbase < prevbase) more_exps[i]++;
     }
 
-    cout << accumulate(more_exps.begin(), more_exps.end(), 0) << "\n";
+    cout << accumulate(more_exps.begin(), more_exps.end(), 0LL) << "\n";
 
     return 0;
 }
