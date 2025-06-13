318091417d87359a709feee2d722748b9bc5f643
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-06-13 13:29:50 +0900

    [Gold I] Title: 회의 장소, Time: 160 ms, Memory: 5148 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31965. 회의 장소/회의 장소.cc
+++ 백준/Gold/31965. 회의 장소/회의 장소.cc
@@ -13,6 +13,9 @@ ll getCost(int idx, int l, int r, vector<ll> &a, vector<ll> &pref) {
 }
 
 int main(int argc, char *argv[]) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     int n, q;
     cin >> n >> q;
 
