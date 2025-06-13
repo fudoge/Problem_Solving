58b4d7193305a2e564ff4a03a1fe1cdebc22e0b0
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-06-13 13:27:19 +0900

    [Gold I] Title: 회의 장소, Time: 84 ms, Memory: 2020 KB, Score: 65 point -BaekjoonHub

---
+++ 백준/Gold/31965. 회의 장소/회의 장소.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+ll getCost(int idx, int l, int r, vector<ll> &a, vector<ll> &pref) {
+    ll leftCnt = idx - l;
+    ll leftSum = pref[idx] - pref[l];
+    ll rightCnt = r - idx;
+    ll rightSum = pref[r + 1] - pref[idx + 1];
+
+    return a[idx] * leftCnt - leftSum + rightSum - a[idx] * rightCnt;
+}
+
+int main(int argc, char *argv[]) {
+    int n, q;
+    cin >> n >> q;
+
+    vector<ll> a(n), pref(n + 1);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        pref[i + 1] = a[i] + pref[i];
+    }
+
+    while (q--) {
+        ll L, R;
+        cin >> L >> R;
+
+        int l = lower_bound(a.begin(), a.end(), L) - a.begin();
+        int r = upper_bound(a.begin(), a.end(), R) - a.begin() - 1;
+
+        if (l >= r) {
+            cout << "0\n";
+            continue;
+        }
+
+        int m = (l + r) >> 1;
+        ll lCost = getCost(l, l, r, a, pref);
+        ll rCost = getCost(r, l, r, a, pref);
+        ll mCost = getCost(m, l, r, a, pref);
+
+        cout << max(lCost, rCost) - mCost << "\n";
+    }
+
+    return 0;
+}
