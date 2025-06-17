3fa7bbf2d21e4ec44b683ccbc5b8fef9fa146136
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-06-17 12:18:19 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2256 ms, Memory: 58344 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#include <queue>
+
+using namespace std;
+
+void solve() {
+    int k;
+    cin >> k;
+
+    unordered_map<int, int> freq;
+    priority_queue<int, vector<int>, greater<>> minHeap;
+    priority_queue<int> maxHeap;
+
+    for (int i = 0; i < k; i++) {
+        char op;
+        int num;
+        cin >> op >> num;
+        if (op == 'I') {
+            freq[num]++;
+            minHeap.emplace(num);
+            maxHeap.emplace(num);
+        } else if (num == -1) {
+            while (!minHeap.empty() && freq[minHeap.top()] <= 0) {
+                minHeap.pop();
+            }
+            if (!minHeap.empty()) {
+                freq[minHeap.top()]--;
+                minHeap.pop();
+            }
+        } else {
+            while (!maxHeap.empty() && freq[maxHeap.top()] <= 0) {
+                maxHeap.pop();
+            }
+            if (!maxHeap.empty()) {
+                freq[maxHeap.top()]--;
+                maxHeap.pop();
+            }
+        }
+    }
+
+    while (!minHeap.empty() && freq[minHeap.top()] <= 0) minHeap.pop();
+    while (!maxHeap.empty() && freq[maxHeap.top()] <= 0) maxHeap.pop();
+    if (minHeap.empty() || maxHeap.empty()) {
+        cout << "EMPTY\n";
+    } else {
+        cout << maxHeap.top() << " " << minHeap.top() << "\n";
+    }
+}
+int main(int argc, char *argv[]) {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
