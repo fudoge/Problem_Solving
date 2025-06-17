d7083c4936446b0c5566b1284fa4c1f5bf225cc1
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-06-17 12:22:18 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1276 ms, Memory: 57856 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-#include <queue>
-
 using namespace std;
 
 void solve() {
@@ -48,6 +46,8 @@ void solve() {
     }
 }
 int main(int argc, char *argv[]) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     int t;
     cin >> t;
     while (t--) {
