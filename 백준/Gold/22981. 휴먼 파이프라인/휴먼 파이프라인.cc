0bd481f82f5fa469ec6aab515fcdc72746a7da78
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-09-18 13:54:05 +0900

    [Gold V] Title: 휴먼 파이프라인, Time: 36 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Gold/22981. 휴먼 파이프라인/휴먼 파이프라인.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(int argc, char *argv[]) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n, k;
+    cin >> n >> k;
+
+    vector<ll> a(n);
+    for (auto &i : a) {
+        cin >> i;
+    }
+
+    sort(a.begin(), a.end());
+    ll ans = 1e18;
+    for (int i = 1; i < n; i++) {
+        ll left = a[0] * (i);
+        ll right = a[i] * (n - i);
+        ll productivity = left + right;
+
+        ll div = k / (productivity);
+        ll rem = k % productivity;
+
+        ll time = div + (rem > 0 ? 1 : 0);
+        if (time < ans) {
+            ans = time;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
