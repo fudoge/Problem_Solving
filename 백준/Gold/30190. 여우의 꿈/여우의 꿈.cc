9031d8680d48e77e35a13eaa4141a44f292e78b2
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-09-25 13:51:37 +0900

    [Gold IV] Title: 여우의 꿈, Time: 64 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/30190. 여우의 꿈/여우의 꿈.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define MAX 1000001
+
+using namespace std;
+using ll = long long;
+
+const ll MOD = 1e9 + 7;
+
+ll f[MAX];
+
+void init() {
+    f[0] = 0;
+    for (int i = 1; i < MAX; i++) {
+        f[i] = (f[i - 1] * 2 + 1) % MOD;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    init();
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+    for (auto& i : a) cin >> i;
+
+    vector<int> state(3);
+    state[k - 1] = n;
+
+    ll ans = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        int src = k;
+        int dst = a[i];
+        if (src == dst) {
+            continue;
+        }
+
+        ans = (ans + f[i] + 1) % MOD;
+        k = 6 - src - dst;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
