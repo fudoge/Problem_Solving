6e25000a6f08c4a4f308da7bac1b8c80954825ae
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-11-01 13:22:31 +0900

    Time: 97 ms (64.45%), Space: 263.5 MB (35.29%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -8,36 +8,24 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-#pragma GCC optimize("03", "unroll-loops");
-static const int __ = [](){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
-
 class Solution {
-public:
+   public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        set<int> s(nums.begin(), nums.end());
-        while (head != nullptr && s.find(head->val) != s.end()) {
-            head = head->next;
-        }
+        unordered_set<int> s(nums.begin(), nums.end());
+        auto dummy = ListNode(0, head);
+        auto curr = head;
+        auto prev = &dummy;
 
-        if (head == nullptr) {
-            return nullptr;
-        }
-        ListNode* curr = head;
-
-        while (curr != nullptr && curr->next != nullptr) {
-            if (s.find(curr->next->val) != s.end()) {
-                curr->next = curr->next->next;
+        while (curr != nullptr) {
+            if (s.find(curr->val) != s.end()) {
+                prev->next = curr->next;
             } else {
-                curr = curr->next;
+                prev = prev->next;
             }
+
+            curr = curr->next;
         }
 
-        return head;
+        return dummy.next;
     }
 };
