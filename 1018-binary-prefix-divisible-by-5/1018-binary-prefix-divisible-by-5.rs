dd5544ca17b540339d8310408e3d5c32de58b054
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-11-24 09:59:06 +0900

    Time: 0 ms (100%), Space: 2.3 MB (77.78%) - LeetHub

---
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn prefixes_div_by5(nums: Vec<i32>) -> Vec<bool> {
+        let n = nums.len();
+        let mut sum = 0;
+        let mut ans = Vec::new();
+
+        for num in nums {
+            sum += num;
+            sum %= 5;
+            if sum == 0 {
+                ans.push(true);
+            } else {
+                ans.push(false);
+            }
+            sum <<= 1;
+        }
+
+        ans
+    }
+}
