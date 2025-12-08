7fe9e7f000296b6fd9ec2e4e5ae2325fa9bdf0c0
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-08 09:34:23 +0900

    Time: 1 ms (95.13%), Space: 7.8 MB (55.75%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -4,11 +4,9 @@ public:
         int ans = 0;
         for(int i = 1; i <= n; i++) {
             for(int j = i+1; j <= n; j++) {
-                for(int k = j+1; k <= n; k++) {
-                    if(i*i+j*j == k*k) {
-                        ans += 2;
-                        break;
-                    }
+                int root = sqrt(i*i + j*j);
+                if(root <= n && root * root == i*i + j*j) {
+                    ans += 2;
                 }
             }
         } 
