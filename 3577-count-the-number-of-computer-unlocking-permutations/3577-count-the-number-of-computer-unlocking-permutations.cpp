46509dbf626cacfc2fe2b76faa9200a9c3689cde
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-10 11:43:24 +0900

    Time: 0 ms (100%), Space: 93.6 MB (19.5%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -2,29 +2,14 @@ using ll = long long;
 
 const int MOD = 1e9 + 7;
 
-ll fact[100001], invfact[100001];
+ll fact[100001];
 bool built = false;
-
-ll bpow(ll base, ll exp) {
-    ll res = 1;
-    while(exp) {
-        if(exp&1) res = (res * base) % MOD;
-        exp >>= 1;
-        base = (base * base) % MOD;
-    }
-    return res;
-}
-
 void build() {
     if (built) return;
     built = true;
     fact[0] = 1;
     for (int i = 1; i <= 100000; i++) {
         fact[i] = fact[i-1] * i % MOD;
-    }
-    invfact[100000] = bpow(fact[100000], MOD-2);
-    for (int i = 99999; i >= 0; i--) {
-        invfact[i] = invfact[i+1] * (i+1) % MOD;
     }
 }
 
