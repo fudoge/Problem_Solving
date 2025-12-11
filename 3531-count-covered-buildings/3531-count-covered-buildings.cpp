bc3134bf3cff60b36cd87963137b5642b373efa6
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-11 09:59:16 +0900

    Time: 24 ms (90.61%), Space: 283.9 MB (78.45%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.cpp
+++ 3531-count-covered-buildings/3531-count-covered-buildings.cpp
@@ -4,44 +4,30 @@ static const int __ = [](){
     cin.tie(0);
     return 0;
 }();
-using ll = long long;
 class Solution {
 public:
-    ll getid(vector<int>& x) {
-        return (ll)x[0] * 100000 + x[1];
-    }
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        int m = buildings.size();
-        unordered_map<ll, bool> ok;
-        vector<vector<int>> xAxis = buildings; 
-        vector<vector<int>> yAxis = buildings; 
-               
-        for(auto building : buildings) {
-            ok[getid(building)] = true;
-        }
+        vector<int> maxX(n+1);
+        vector<int> minX(n+1, n+1);
+        vector<int> maxY(n+1);
+        vector<int> minY(n+1, n+1);
 
-        sort(xAxis.begin(), xAxis.end(), [](const auto &a, const auto &b){
-            if(a[0] == b[0]) {
-                return a[1] < b[1];
-            }    
-            return a[0] < b[0];
-        });
-        sort(yAxis.begin(), yAxis.end(), [](const auto &a, const auto &b){
-            if(a[1] == b[1]) {
-                return a[0] < b[0];
-            }    
-            return a[1] < b[1];
-        });
-
-        for(int i = 0; i < m; i++) {
-            if(i == 0 || i == m-1 || xAxis[i-1][0] != xAxis[i][0] || xAxis[i+1][0] != xAxis[i][0]) ok[getid(xAxis[i])] = false;
-            if(i == 0 || i == m-1 || yAxis[i-1][1] != yAxis[i][1] || yAxis[i+1][1] != yAxis[i][1]) ok[getid(yAxis[i])] = false;
+        for(const auto &b : buildings) {
+            int x = b[0], y = b[1];
+            maxX[x] = max(maxX[x], y);
+            minX[x] = min(minX[x], y);
+            maxY[y] = max(maxY[y], x);
+            minY[y] = min(minY[y], x);
         }
 
         int ans = 0;
-        for(const auto &[_, v] : ok) {
-            ans += v;
+        for(const auto &b : buildings) {
+            int x = b[0], y = b[1];
+            if(y > minX[x] && y < maxX[x] && x > minY[y] && x < maxY[y]) {
+                ans++;
+            }
         }
+
         return ans;
     }
 };
