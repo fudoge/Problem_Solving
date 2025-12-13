c84d14dc8d23b8150b134e43e6f2c181952d6f24
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-13 10:52:24 +0900

    Time: 3 ms (81.55%), Space: 68.5 MB (80.69%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.cpp
+++ 3606-coupon-code-validator/3606-coupon-code-validator.cpp
@@ -1,20 +1,7 @@
-#pragma GCC optimize("O3", "unroll-loops");
-static const int __ == [](){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    return 0;
-}();
 class Solution {
 public:
-    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
-        unordered_map<string, int> level = {
-            { "electronics", 1 },
-            { "grocery", 2 },
-            { "pharmacy", 3 },
-            { "restaurant", 4 },
-        };
-        vector<string> ans;
-        vector<vector<string>> coupons;
+    vector<string> addCoupon(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive, string& acceptBL) {
+        vector<string> res;
         for(int i = 0; i < code.size(); i++) {
             if(code[i].size() < 1) {
                 continue;
@@ -27,22 +14,22 @@ public:
                 }
             }
             if(flag) continue;
-            if(!level[businessLine[i]]) continue;
+            if(businessLine[i] != acceptBL) continue;
             if(!isActive[i]) continue;
-            coupons.push_back({code[i], businessLine[i]});
+            res.emplace_back(code[i]);
         } 
 
-        sort(coupons.begin(), coupons.end(), [&](const auto &a, const auto &b){
-            int la = level[a[1]];
-            int lb = level[b[1]];
-            if(la == lb) {
-                return a[0] < b[0];
+        return res;
+    }
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        vector<string> ans;
+        vector<string> acceptBLs = {"electronics", "grocery", "pharmacy", "restaurant"};
+        for(auto &abl : acceptBLs) {
+            auto res = addCoupon(code, businessLine, isActive, abl);
+            sort(res.begin(), res.end());
+            for(string &s : res) {
+                ans.emplace_back(s);
             }
-            return la < lb;
-        });
-
-        for(auto c : coupons) {
-            ans.emplace_back(c[0]);
         }
 
         return ans;
