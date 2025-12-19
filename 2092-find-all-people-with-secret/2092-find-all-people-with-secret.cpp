b12042dd9c468fec6107b7964a536cf924855f91
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-20 03:02:16 +0900

    Time: 106 ms (90.24%), Space: 170.1 MB (87.8%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,71 +1,44 @@
 class Solution {
 public:
+    int find(vector<int>& parent, int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent, parent[x]);
+    }
+    void unite(vector<int>& parent, int x, int y) {
+        x = find(parent, x);
+        y = find(parent, y);
+
+        if(x == y) return;
+        if(x < y) {
+            parent[y] = x;
+        } else {
+            parent[x] = y;
+        }
+    }
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
         int m = meetings.size();
         sort(meetings.begin(), meetings.end(), [](const auto &a, const auto &b){
             return a[2] < b[2];
         });
 
-        vector<bool> knows(n);
-        knows[0] = true;
-        knows[firstPerson] = true;
+        vector<int> parent(n);
+        iota(parent.begin(), parent.end(), 0);
+        unite(parent, 0, firstPerson);
 
-        int i = 0;
-        while(i < m) {
-            int time = meetings[i][2];
-
-            unordered_map<int, int> parent;
-
-            auto ensure = [&](int x) {
-                if(!parent.count(x)) parent[x] = x;
-            };
-
-            function<int(int)> find = [&](int x) {
-                if(parent[x] == x) return x;
-                return parent[x] = find(parent[x]);
-            };
-
-            auto unite = [&](int x, int y) {
-                x = find(x);
-                y = find(y);
-                if(x != y) parent[y] = x;
-            };
-
-            int j = i;
-            while(j < m && meetings[j][2] == time) {
-                int x = meetings[j][0];
-                int y = meetings[j][1];
-                ensure(x);
-                ensure(y);
-                unite(x, y);
-                j++;
+        for(int l = 0, r = 0; l < meetings.size();) {
+            for(; r < meetings.size() && meetings[l][2] == meetings[r][2]; ++r) {
+                unite(parent, meetings[r][0], meetings[r][1]);
             }
 
-            unordered_set<int> goodRoots;
-            for(int k = i; k < j; k++) {
-                int x = meetings[k][0];
-                int y = meetings[k][1];
-                if(knows[x] || knows[y]) {
-                    goodRoots.insert(find(x));
-                    goodRoots.insert(find(y));
-                }
+            for(int s = find(parent, 0); l < r; ++l) {
+                if(find(parent, meetings[l][0]) != s) parent[meetings[l][0]] = meetings[l][0];
+                if(find(parent, meetings[l][1]) != s) parent[meetings[l][1]] = meetings[l][1];
             }
-
-            for(int k = i; k < j; k++) {
-                int x = meetings[k][0];
-                int y = meetings[k][1];
-                if(goodRoots.count(find(x))) {
-                    knows[x] = true;
-                    knows[y] = true;
-                }
-            }
-
-            i = j;
         }
 
         vector<int> ans;
         for(int i = 0; i < n; i++) {
-            if(knows[i]) ans.emplace_back(i);
+            if(parent[i] == 0) ans.emplace_back(i);
         }
 
         return ans;
