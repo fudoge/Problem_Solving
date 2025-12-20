45331833f1b5660578142f9af944a9e88c1b4abc
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-20 11:24:51 +0900

    Time: 3 ms (24.32%), Space: 9.2 MB (5.41%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.go
@@ -0,0 +1,16 @@
+func minDeletionSize(strs []string) int {
+    m := len(strs[0])
+    n := len(strs)
+
+    ans := 0
+    for j := 0; j < m; j++ {
+        for i := 1; i < n; i++ {
+            if strs[i][j] < strs[i-1][j] {
+                ans++
+                break
+            }
+        }
+    }
+
+    return ans
+}
