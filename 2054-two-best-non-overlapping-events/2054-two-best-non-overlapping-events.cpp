8f3ada3ebb5334cad1312387cba8901459603b30
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-23 10:37:59 +0900

    Time: 131 ms (45.56%), Space: 177.2 MB (42.09%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -8,32 +8,25 @@ class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
         unordered_map<int, int> best;
-        set<int> ends;
+        vector<int> ends = {0};
         sort(events.begin(), events.end(), [](const auto &a, const auto &b){
             return a[1] < b[1];
         });
 
-        ends.insert(0);
         int ans = 0;
         for(const auto &event : events) {
             int s = event[0];
             int e = event[1];
             int v = event[2];
 
-            auto lastEndIt = ends.lower_bound(s);
-            if(lastEndIt != ends.begin()) {
-                --lastEndIt;
-            }
+            auto lastEndIt = lower_bound(ends.begin(), ends.end(), s);
+            if(lastEndIt != ends.begin()) { --lastEndIt; }
             int lastEnd = *lastEndIt;
             ans = max(ans, best[lastEnd] + v);
 
-            ends.insert(e);
+            if(ends.back() != e) { ends.emplace_back(e); }
             best[e] = max(best[e], v);
-            auto it = ends.lower_bound(e);
-            if(it != ends.begin()) {
-                --it;
-            }
-            best[e] = max(best[e], best[*it]);
+            best[e] = max(best[e], best[ends[ends.size()-2]]);
         }
 
         return ans;
