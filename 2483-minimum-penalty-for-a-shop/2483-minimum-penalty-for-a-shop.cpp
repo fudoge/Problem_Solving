578002a7770b4ef62c17c050cc90dfbdb21995cd
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-26 12:13:21 +0900

    Time: 3388 ms (5.03%), Space: 17.6 MB (53.22%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int n = customers.size();
+        int y = 0;
+        for(int i = 0; i < n; i++) {
+            if(customers[i] == 'Y') {
+                y++;
+            }
+        } 
+        vector<int> dp(n+1);
+        dp[0] = y;
+        
+        int ans = 0;
+        for(int i = 1; i <= n; i++) {
+            dp[i] = dp[i-1];
+            if(customers[i-1] == 'Y') {
+                dp[i]--;
+            } else {
+                dp[i]++;
+            }
+            if(dp[i] < dp[ans]) {
+                ans = i;
+            }
+            cout << i << " " << dp[i] << "\n";
+        }
+
+        return ans;
+    }
+};
