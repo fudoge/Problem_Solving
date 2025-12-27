1c4fb5ec6fff791088fedb7de8efef0a7d752006
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2025-12-27 13:02:54 +0900

    Time: 71 ms (61.03%), Space: 98.2 MB (70.22%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    int mostBooked(int n, vector<vector<int>> &meetings) {
+    int mostBooked(int n, vector<vector<int>>& meetings) {
         vector<int> ans(n, 0);
-        priority_queue<int, vector<int>, greater<int>> available;
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> busy;
-        
-        for (int i = 0; i < n; i++) {
+        priority_queue<int, vector<int>, greater<>> available;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> busy;
+
+        for(int i = 0; i < n; i++) {
             available.push(i);
         }
-        
+
         sort(meetings.begin(), meetings.end());
+        for(const auto &m : meetings) {
+            int s = m[0];
+            int e = m[1];
 
-        for (auto &&meeting : meetings) {
-            int start = meeting[0], end = meeting[1];
-
-            // 시간지난 회의실 비우기
-            while (!busy.empty() && busy.top().first <= start) {
+            while(!busy.empty() && busy.top().first <= s) {
                 available.push(busy.top().second);
                 busy.pop();
             }
 
-            // 만약 사용가능한 곳이 있다면
-            if (!available.empty()) {
+            if(!available.empty()) {
                 int top = available.top();
                 ans[top]++;
                 available.pop();
-                busy.push({end, top});
-            // 없다면, 다음 빠지는곳에 미리 예약걸어놓기
+                busy.push({e, top});
             } else {
                 auto top = busy.top();
-                int end1 = top.first, index = top.second;
-
-                ans[index]++;
+                int pend = top.first;
+                int room = top.second;
+                ans[room]++;
                 busy.pop();
-                busy.push({top.first + end - start, index});
+                busy.push({top.first + e - s, room});
             }
         }
 
