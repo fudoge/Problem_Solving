75df9d2f2905aeb42a79a285f172c967c0804cf2
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2026-01-05 09:12:43 +0900

    Time: 2 ms (20%), Space: 9.4 MB (40%) - LeetHub

---
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.go
@@ -0,0 +1,21 @@
+func maxMatrixSum(matrix [][]int) int64 {
+    n := len(matrix)
+    maximumNeg := math.MaxInt
+    ans := int64(0)
+    cnt := 0
+
+    for i := 0; i < n; i++ {
+        for j := 0; j < n; j++ {
+            if matrix[i][j] < 0 {
+                cnt++
+            }
+            maximumNeg = min(maximumNeg, int(math.Abs(float64(matrix[i][j]))));
+            ans += int64(math.Abs(float64(matrix[i][j])))
+        }
+    }
+
+    if cnt % 2 == 1 {
+        ans -= 2 * int64(maximumNeg)
+    }
+    return ans
+}
