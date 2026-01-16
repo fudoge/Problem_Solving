94474c0cd85b926f05e05e846d96c8c80fb79aea
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2026-01-16 19:18:05 +0900

    Time: 1147 ms (57.28%), Space: 250.4 MB (92.23%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,36 +1,40 @@
+#pragma GCC optimize("O3", "unroll-loops");
+static const int __ = [](){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    return 0;
+}();
 const int MOD = 1e9+7;
 class Solution {
 public:
-    unordered_set<int> getIntervals(vector<int>& fences) {
-        unordered_set<int> intervals;
+    vector<int> getIntervals(vector<int>& fences) {
+        vector<int> intervals;
         for(int i = 0; i < fences.size()-1; i++) {
             for(int j = i+1; j < fences.size(); j++) {
-                intervals.insert(fences[j] - fences[i]);
+                intervals.emplace_back(fences[j] - fences[i]);
             }
         }
+        sort(intervals.begin(), intervals.end());
+        intervals.erase(unique(intervals.begin(), intervals.end()), intervals.end());
         return intervals;
     }
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
-        hFences.push_back(1);
-        hFences.push_back(m);
-        vFences.push_back(1);
-        vFences.push_back(n);
+        hFences.emplace_back(1);
+        hFences.emplace_back(m);
+        vFences.emplace_back(1);
+        vFences.emplace_back(n);
         sort(hFences.begin(), hFences.end());
         sort(vFences.begin(), vFences.end());
         
-        unordered_set<int> hIntervals = getIntervals(hFences);
-        unordered_set<int> vIntervals = getIntervals(vFences);
+        vector<int> hIntervals = getIntervals(hFences);
+        vector<int> vIntervals = getIntervals(vFences);
         
-        vector<int> cand;
-        for(int elem : hIntervals) {
-            if(vIntervals.find(elem) != vIntervals.end()) {
-                cand.push_back(elem);
-            }
+        long long side = -1;
+        for(const auto &elem : hIntervals) {
+            auto it = lower_bound(vIntervals.begin(), vIntervals.end(), elem) - vIntervals.begin();
+            if(vIntervals[it] == elem) side = max(side, (long long)elem);
         }
-        if(cand.empty()) return -1;
-        sort(cand.begin(), cand.end());
-        long long side = cand[cand.size()-1];
-
+        if(side == -1) return -1;
         return side * side % MOD;
     }
 };
