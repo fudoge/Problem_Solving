576fa757262735318c606f5e03f4b30111885ec0
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2026-01-31 13:07:19 +0800

    Time: 0 ms (100%), Space: 19.7 MB (88.8%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        char ans = '|';
-        for(const auto &let : letters) {
-            if(let > target) {
-                ans = min(ans, let);
-            }
-        }
-
-        return ans == '|'? letters[0] : ans;
+        auto it = upper_bound(letters.begin(), letters.end(), target);
+        return it == letters.end() ? letters[0] : *it;
     }
 };
