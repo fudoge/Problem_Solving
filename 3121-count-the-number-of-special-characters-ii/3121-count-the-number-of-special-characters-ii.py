71261e040527a72fd6e0e63c722c89400802d99b
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2026-05-27 09:33:54 +0900

    Time: 42 ms (100%), Space: 21.4 MB (62.94%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,20 +1,8 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        lower_last = [1e9] * 26
-        upper_first = [-1] * 26
-
-        for i, ch in enumerate(word):
-            if ch >= 'a' and ch <= 'z':
-                lower_last[ord(ch) - ord('a')] = i
-            else:
-                ch_ord = ord(ch)-ord('A')
-                if upper_first[ch_ord] == -1:
-                    upper_first[ch_ord] = i
-            
-        
         ans = 0
-        for i in range(0, 26):
-            if lower_last[i] < upper_first[i]:
-                ans += 1
+        for ch, CH in zip(ascii_lowercase, ascii_uppercase):
+            if ch not in word or CH not in word: continue
+            ans += word.rfind(ch) < word.find(CH)
         
         return ans
