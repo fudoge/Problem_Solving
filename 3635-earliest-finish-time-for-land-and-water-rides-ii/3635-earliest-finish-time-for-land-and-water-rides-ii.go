afd0114d3e3eb1799bcbb987593fa3fec2b316d2
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2026-06-04 02:38:57 +0900

    Time: 232 ms (0%), Space: 16.4 MB (16.67%) - LeetHub

---
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.go
@@ -0,0 +1,87 @@
+type Item struct {
+    startTime      int
+    duration       int
+    idealEndTime   int
+}
+
+func earliestFinishTime(landStartTime []int, landDuration []int, waterStartTime []int, waterDuration []int) int {
+    n := len(landStartTime)         
+    m := len(waterStartTime)         
+
+    landRide := make([]*Item, n)
+    waterRide := make([]*Item, m)
+
+    for i := 0; i < n; i++ {
+        landRide[i] = &Item{
+            startTime:    landStartTime[i],
+            duration:     landDuration[i],
+            idealEndTime: landStartTime[i] + landDuration[i],
+        }
+    }
+
+    for i := 0; i < m; i++ {
+        waterRide[i] = &Item{
+            startTime:    waterStartTime[i],
+            duration:     waterDuration[i],
+            idealEndTime: waterStartTime[i] + waterDuration[i],
+        }
+    }
+
+    slices.SortFunc(landRide, func(a, b *Item) int {
+        return cmp.Compare(a.startTime, b.startTime)
+    })
+    slices.SortFunc(waterRide, func(a, b *Item) int {
+        return cmp.Compare(a.startTime, b.startTime)
+    })
+
+    bestLandFinish := make([]int, n)
+    bestLandFinish[n-1] = landRide[n-1].idealEndTime
+    bestWaterFinish := make([]int, m)
+    bestWaterFinish[m-1] = waterRide[m-1].idealEndTime
+    for i := n-2; i >= 0; i--  {
+        bestLandFinish[i] = min(bestLandFinish[i+1], landRide[i].idealEndTime)
+    }
+    for i := m-2; i >= 0; i--  {
+        bestWaterFinish[i] = min(bestWaterFinish[i+1], waterRide[i].idealEndTime)
+    }
+
+    minLandDuration := make([]int, n)
+    minLandDuration[0] = landRide[0].duration
+    minWaterDuration := make([]int, m)
+    minWaterDuration[0] = waterRide[0].duration
+    for i := 1; i < n; i++ {
+        minLandDuration[i] = min(minLandDuration[i-1], landRide[i].duration)
+    }
+    for i := 1; i < m; i++ {
+        minWaterDuration[i] = min(minWaterDuration[i-1], waterRide[i].duration)
+    }
+
+    ans := math.MaxInt
+    for i := 0; i < n; i++ {
+        firstEnd := landRide[i].idealEndTime
+        nextAvailableIdx := sort.Search(len(waterRide), func(j int) bool {
+            return waterRide[j].startTime >= firstEnd
+        })
+        if nextAvailableIdx > 0 {
+            ans = min(ans, firstEnd + minWaterDuration[nextAvailableIdx-1])
+        }
+        if nextAvailableIdx < m {
+            ans = min(ans, bestWaterFinish[nextAvailableIdx])
+        }
+    }
+
+    for i := 0; i < m; i++ {
+        firstEnd := waterRide[i].idealEndTime
+        nextAvailableIdx := sort.Search(len(landRide), func(j int) bool {
+            return landRide[j].startTime >= firstEnd
+        })
+        if nextAvailableIdx > 0 {
+            ans = min(ans, firstEnd + minLandDuration[nextAvailableIdx-1])
+        }
+        if nextAvailableIdx < n {
+            ans = min(ans, bestLandFinish[nextAvailableIdx])
+        }
+    }
+
+    return ans
+}
