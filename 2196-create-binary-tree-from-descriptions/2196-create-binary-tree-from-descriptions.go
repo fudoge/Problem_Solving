6fab77e1ecbde30b2e0c54621c7caf1a294e9c99
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2026-06-07 19:54:19 +0900

    Time: 65 ms (63.16%), Space: 10.6 MB (47.37%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.go
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.go
@@ -7,37 +7,33 @@
  * }
  */
 func createBinaryTree(descriptions [][]int) *TreeNode {
-    nodes := make([]*TreeNode, 100001)
-    indegree := make([]int, 100001)
-    isNodeExists := make(map[int]bool)
-    
-    for i := 0; i < 100001; i++ {
-        nodes[i] = &TreeNode{
-            Val: i,
-            Left: nil,
-            Right: nil,
-        }
-    }
+    nodes := make(map[int]*TreeNode)
+    root := 0 
     
     for _, desc := range descriptions {
         p := desc[0]
         c := desc[1]
         isLeft := desc[2]
-        isNodeExists[p] = true
-        isNodeExists[c] = true
-        indegree[c]++
-        
-        if isLeft == 1 {
-            nodes[p].Left = nodes[c]
-        } else {
-            nodes[p].Right = nodes[c]
-        }
-    }
 
-    for k, _ := range isNodeExists {
-        if indegree[k] == 0 {
-            return nodes[k]
+        if _, exists := nodes[p]; !exists {
+            nodes[p] = &TreeNode {
+                Val: p,
+            }
+            root += p
         }
+        if _, exists := nodes[c]; !exists {
+            nodes[c] = &TreeNode {
+                Val: c,
+            }
+            root += c
+        }
+
+        if isLeft == 1 { 
+            nodes[p].Left = nodes[c] 
+        } else { 
+            nodes[p].Right = nodes[c] 
+        }
+        root -= c
     }
-    return nil
+    return nodes[root]
 }
