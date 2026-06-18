eeb6ae67e0e5596802b2193f6a48bb18f3e8c8a0
Chaewoon Kang <77766067+fudoge@users.noreply.github.com>
2026-06-18 11:48:46 +0900

    Time: 2 ms (0%), Space: 3.9 MB (60%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.go
@@ -0,0 +1,14 @@
+const MOD int = 360
+
+func angleClock(hour int, minutes int) float64 {
+    hDeg := 30.0 * float64(hour) + 0.5 * float64(minutes)
+    mDeg := 6.0 * float64(minutes)
+    if hDeg >= 360.0 {
+        hDeg -= 360.0
+    }
+
+    lo := min(hDeg, mDeg)
+    hi := max(hDeg, mDeg)
+
+    return min(hi - lo, 360.0 - hi + lo)
+}
